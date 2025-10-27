c3de065d66df463b037d804f2194eeeb5359e68e
Nam Nguyen Hai <doluadao1995@gmail.com>
2025-10-27 15:06:20 +0100

    Add strapi-import-export plugin for data import/export functionality; update package.json and pnpm-lock.yaml to include new dependencies; configure plugin in plugins.ts and set permissions in admin role configuration.

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,9 +1,12 @@
 export default ({ env }) => ({
     "gen-types": {
-        enabled: true,
-        config: {
-          outputLocation: "types/generated/types.ts",
-          singleFile: true,
-        },
+      enabled: true,
+      config: {
+        outputLocation: "types/generated/types.ts",
+        singleFile: true,
       },
+    },
+    'strapi-import-export': {
+      enabled: true,
+    },
 });
